91a601d7bf4069133e818ac4573633bcfd47630f
ddehueck <d.dehueck@gmail.com>
2026-08-21 15:35:19 -0400

    clarification

--- crates/local/src/lib.rs
+++ crates/local/src/lib.rs
@@ -6,6 +6,8 @@ mod db;
 mod service;
 
 // This is the single entrypoint for the local Zygo service.
+pub use service::ZygoLocalService;
+
+// Type re-exports only for convenience
 pub use db::{KvRepository, Tag, WorkflowRun, WorkflowRunRepository};
 pub use repos::Repos;
-pub use service::ZygoLocalService;
